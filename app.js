71cf05d3c8dbbd9940e715fad1a612e6201baadc
Rafael Figueiredo <rafaelnegreirosfigueiredo@gmail.com>
2020-06-08 09:06:57 -0300

    Finished

--- app.js
+++ app.js
@@ -4,38 +4,40 @@ document.getElementById("loan-form").addEventListener("submit", function (e) {
 
   document.getElementById("loading").style.display = "block";
 
-  setTimeout(calculateResults, 2000);
+  setTimeout(calculateResults, 500);
 
   e.preventDefault();
 });
 
 function calculateResults() {
   console.log("calculating...");
-}
-const amount = document.getElementById("amount");
-const interest = document.getElementById("interest");
-const years = document.getElementById("years");
-const monthlyPayment = document.getElementById("monthly-payment");
-const totalPayment = document.getElementById("total-payment");
-const totalInterest = document.getElementById("total-interest");
 
-const principal = parseFloat(amount.value);
-const calculatedInterest = parseFloat(interest.value) / 100 / 12;
-const calculatedPayments = parseFloat(years.value) * 12;
+  const amount = document.getElementById("amount");
+  const interest = document.getElementById("interest");
+  const years = document.getElementById("years");
+  const monthlyPayment = document.getElementById("monthly-payment");
+  const totalPayment = document.getElementById("total-payment");
+  const totalInterest = document.getElementById("total-interest");
 
-const x = Math.pow(1 + calculatedInterest, calculatedPayments);
-const monthly = (principal * x * calculatedInterest) / (x - 1);
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
 
-if (isFinite(monthly)) {
-  monthlyPayment.value = monthly.toFixed(3);
-  totalPayment.value = (monthly * calculatedPayments).toFixed(2);
-  totalInterest.value = (monthly * calculatedPayments - principal).toFixed(3);
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
 
-  document.getElementById("results").style.display = "block";
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(3);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = (monthly * calculatedPayments - principal).toFixed(3);
 
-  document.getElementById("loading").style.display = "none";
-} else {
-  showError("Please check your numbers");
+    document.getElementById("results").style.display = "block";
+
+    document.getElementById("loading").style.display = "none";
+  } else {
+    console.log("clicked");
+    showError("Please check your numbers");
+  }
 }
 
 function showError(error) {
